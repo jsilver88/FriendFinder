50269503cb67c999278e44260f37da0bf72072ce
Jared Silver <jrsilver88@gmail.com>
2020-05-04 20:09:41 -0700

    home/survey html created. htmlroutes created.

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+let closeFriends = [
+    {
+        name: "Tom Brady",
+        photo: "https://media1.s-nbcnews.com/j/newscms/2019_34/2983126/190823-tom-brady-se-1205p_37489191b47c4557091dbec6300024c2.fit-2000w.jpg",
+        scores: 
+    }
+]
